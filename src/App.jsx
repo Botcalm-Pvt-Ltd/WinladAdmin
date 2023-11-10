61751595093f3d913193998930cdc49da7a10bff
NimsaraBotCalm <nimsara@botcalm.com>
2023-11-10 14:49:16 +0530

    AdminDash

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AdminDashboard from './pages/AdminDashboard'
 
 function App() {
 
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AdminDashboard/>}/>
+    </Routes>
+    </BrowserRouter>
     
     </>
   )
---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SideNavBar from './SideNavBar'
+
+const AdminDashboard = () => {
+  return (
+    <div className='flex items-start justify-center'>
+        <SideNavBar/>
+        <div className='w-3/4'>
+
+        </div>
+    </div>
+  )
+}
+
+export default AdminDashboard
