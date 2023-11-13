42af53b4a562f13c02e50e5eb4eb314f3624d565
NimsaraBotCalm <nimsara@botcalm.com>
2023-11-13 17:48:00 +0530

    Responsive optimizations

--- src/components/BackgroundBlackSquare/BackSquare.jsx
+++ src/components/BackgroundBlackSquare/BackSquare.jsx
@@ -3,7 +3,7 @@ import hiddenCar from '../../assets/hiddenCar.png';
 
 const BackSquare = () => {
     return (
-        <div className='absolute top-0 right-0 rounded-bl-3xl rounded-br-3xl  bg-black w-1/2 lg:w-2/3 h-1/3 lg:h-1/2'>
+        <div className='absolute top-0 right-0 rounded-bl-3xl rounded-br-3xl  bg-black w-1/2 lg:w-2/3 h-2/6 lg:h-1/2'>
             <img src={hiddenCar} className='w-50 mt-auto absolute bottom-0 left-0' />
         </div>
     )
--- src/components/GreenCard.jsx
+++ src/components/GreenCard.jsx
@@ -3,11 +3,11 @@ import { AiOutlineLineChart } from 'react-icons/ai'
 
 const GreenCard = ({icon = <AiOutlineLineChart/> , text, subtext}) => {
   return (
-    <div className='bg-green-300 flex items-center justify-center px-3 py-2 rounded-xl w-40 z-10  overflow-hidden leading-none '>
-        <div className='w-1/3 text-3xl'>
+    <div className='bg-green-300 flex items-center justify-center px-3 py-2 rounded-xl w-52 z-10  overflow-hidden leading-none hover:-translate-y-1 transition-all cursor-pointer'>
+        <div className='w-1/4 text-3xl'>
            {icon}
         </div>
-        <div className='w-2/3 whitespace-nowrap overflow-hidden'>
+        <div className='w-3/4 whitespace-nowrap overflow-hidden'>
             <h5>{text}</h5>
             <p className='text-xs text-gray-500'>{subtext}</p>
         </div>
